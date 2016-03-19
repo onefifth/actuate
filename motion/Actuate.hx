87b4ecec7597e9149e75522cdde5e560f981be3f
⅕ Zander <zander.milroy@gmail.com>
2016-03-18 22:22:56 -0400

    Adds isTweening<T>

--- motion/Actuate.hx
+++ motion/Actuate.hx
@@ -19,16 +19,16 @@ import flash.display.DisplayObject;
 
 
 class Actuate {
-	
-	
+
+
 	public static var defaultActuator:Class<IGenericActuator> = SimpleActuator;
 	public static var defaultEase:IEasing = Expo.easeOut;
 	private static var targetLibraries = new ObjectMap<Dynamic, Array<IGenericActuator>> ();
 	#if neko
 	private static var methodLibraries = new FunctionMap<Dynamic, Array<IGenericActuator>> ();
 	#end
-	
-	
+
+
 	/**
 	 * Copies properties from one object to another. Conflicting tweens are stopped automatically
 	 * @example		<code>Actuate.apply (MyClip, { alpha: 1 } );</code>
@@ -38,90 +38,108 @@ class Actuate {
 	 * @return		The current actuator instance, which can be used to apply properties like onComplete or onUpdate handlers
 	 */
 	/*@:generic*/ public static function apply<T> (target:T, properties:Dynamic, customActuator:Class<GenericActuator<T>> = null):GenericActuator<T> {
-		
+
 		stop (target, properties);
-		
+
 		if (customActuator == null) {
-			
+
 			customActuator = cast defaultActuator;
-			
+
 		}
-		
+
 		var actuator:GenericActuator<T> = Type.createInstance (customActuator, [ target, 0, properties ]);
 		actuator.apply ();
-		
+
 		return actuator;
-		
+
 	}
-	
-	
+
+
 	#if (flash || nme || openfl)
-	
+
 	/**
-	 * Creates a new effects tween 
+	 * Creates a new effects tween
 	 * @param	target		The object to tween
 	 * @param	duration		The length of the tween in seconds
 	 * @param	overwrite		Sets whether previous tweens for the same target and properties will be overwritten (Default is true)
 	 * @return		An EffectsOptions instance, which is used to select the kind of effect you would like to apply to the target
 	 */
 	public static function effects (target:DisplayObject, duration:Float, overwrite:Bool = true):EffectsOptions {
-		
+
 		return new EffectsOptions (target, duration, overwrite);
-		
+
 	}
-	
+
 	#end
-	
-	
+
+
 	private static function getLibrary<T> (target:T, allowCreation:Bool = true):Array<IGenericActuator> {
-		
+
 		#if neko
-		
+
 		if (Reflect.isFunction (target)) {
-			
+
 			if (!methodLibraries.exists (target) && allowCreation) {
-				
+
 				methodLibraries.set (target, new Array<IGenericActuator> ());
-				
+
 			}
-			
+
 			return methodLibraries.get (target);
-			
+
 		}
-			
+
 		#end
-		
+
 		if (!targetLibraries.exists (target) && allowCreation) {
-			
+
 			targetLibraries.set (target, new Array<IGenericActuator> ());
-			
+
 		}
-		
+
 		return targetLibraries.get (target);
-		
+
 	}
-	
-	
+
+
 	/**
-	 * Checks if Actuate has any active tweens 
+	 * Checks if Actuate has any active tweens
 	 * @return		Whether Actuate is active
 	 */
 	public static function isActive ():Bool {
-		
+
 		var result = false;
-		
+
 		for (library in targetLibraries) {
-			
+
 			result = true;
 			break;
-			
+
 		}
-		
+
 		return result;
-		
+
 	}
-	
-	
+
+
+	/**
+	 * Checks if a target is being tweened
+	 * @param	target	The object to check
+	 * @return		Whether or not the target is tweening.
+	 */
+	public static function isTweening<T> (target:T):Bool {
+		#if neko
+
+		if (Reflect.isFunction (target)) {
+			return methodLibraries.exists (target));
+		}
+
+		#end
+
+		return targetLibraries.exists (target);
+	}
+
+
 	/**
 	 * Creates a new MotionPath tween
 	 * @param	target		The object to tween
@@ -131,233 +149,233 @@ class Actuate {
 	 * @return		The current actuator instance, which can be used to apply properties like ease, delay, onComplete or onUpdate
 	 */
 	/*@:generic*/ public static function motionPath<T> (target:T, duration:Float, properties:Dynamic, overwrite:Bool = true):GenericActuator<T> {
-		
+
 		return tween (target, duration, properties, overwrite, MotionPathActuator);
-		
+
 	}
-	
-	
+
+
 	/**
 	 * Pauses tweens for the specified target objects
 	 * @param	... targets		The target objects which will have their tweens paused. Passing no value pauses tweens for all objects
 	 */
 	///*@:generic*/ public static function pause (... targets:Array):void {
 	/*@:generic*/ public static function pause<T> (target:T):Void {
-		
+
 		if (Std.is (target, IGenericActuator)) {
-			
+
 			var actuator:IGenericActuator = cast target;
 			actuator.pause ();
-			
+
 		} else {
-			
+
 			var library = getLibrary (target, false);
-			
+
 			if (library != null) {
-				
+
 				for (actuator in library) {
-					
+
 					actuator.pause ();
-					
+
 				}
-				
+
 			}
-			
+
 		}
-		
+
 	}
-	
-	
+
+
 	public static function pauseAll ():Void {
-		
+
 		#if neko
-		
+
 		for (library in methodLibraries) {
-			
+
 			for (actuator in library) {
-				
+
 				actuator.pause ();
-				
+
 			}
-			
+
 		}
-		
+
 		#end
-		
+
 		for (library in targetLibraries) {
-			
+
 			for (actuator in library) {
-				
+
 				actuator.pause ();
-				
+
 			}
-			
+
 		}
-		
+
 	}
-	
-	
+
+
 	/**
 	 * Resets Actuate by stopping and removing tweens for all objects
 	 */
 	public static function reset ():Void {
-		
+
 		#if neko
-		
+
 		for (library in methodLibraries) {
-			
+
 			var i = library.length - 1;
-			
+
 			while (i >= 0) {
-				
+
 				library[i].stop (null, false, false);
 				i--;
-				
+
 			}
-			
+
 		}
-		
+
 		methodLibraries = new FunctionMap<Dynamic, Array<IGenericActuator>> ();
-		
+
 		#end
-		
+
 		for (library in targetLibraries) {
-			
+
 			var i = library.length - 1;
-			
+
 			while (i >= 0) {
-				
+
 				library[i].stop (null, false, false);
 				i--;
-				
+
 			}
-			
+
 		}
-		
+
 		targetLibraries = new ObjectMap<Dynamic, Array<IGenericActuator>> ();
-		
+
 	}
-	
-	
+
+
 	/**
 	 * Resumes paused tweens for the specified target objects
 	 * @param	... targets		The target objects which will have their tweens resumed. Passing no value resumes tweens for all objects
 	 */
 	/*@:generic*/ public static function resume<T> (target:T):Void {
-		
+
 		if (Std.is (target, IGenericActuator)) {
-			
+
 			var actuator:IGenericActuator = cast target;
 			actuator.resume ();
-			
+
 		} else {
-			
+
 			var library = getLibrary (target, false);
-			
+
 			if (library != null) {
-				
+
 				for (actuator in library) {
-					
+
 					actuator.resume ();
-					
+
 				}
-				
+
 			}
-			
+
 		}
-		
+
 	}
-	
-	
+
+
 	public static function resumeAll ():Void {
-		
+
 		#if neko
-		
+
 		for (library in methodLibraries) {
-			
+
 			for (actuator in library) {
-				
+
 				actuator.resume ();
-				
+
 			}
-			
+
 		}
-		
+
 		#end
-		
+
 		for (library in targetLibraries) {
-			
+
 			for (actuator in library) {
-				
+
 				actuator.resume ();
-				
+
 			}
-			
+
 		}
-		
+
 	}
-	
-	
+
+
 	/**
 	 * Stops all tweens for an individual object
 	 * @param	target		The target object which will have its tweens stopped, or a generic actuator instance
 	 * @param	properties		A string, array or object which contains the properties you wish to stop, like "alpha", [ "x", "y" ] or { alpha: null }. Passing no value removes all tweens for the object (Optional)
-	 * @param	complete		If tweens should apply their final target values before stopping. Default is false (Optional) 
+	 * @param	complete		If tweens should apply their final target values before stopping. Default is false (Optional)
 	 * @param	sendEvent	If a complete() event should be dispatched for the specified target. Default is true (Optional)
 	 */
 	/*@:generic*/ public static function stop<T> (target:T, properties:Dynamic = null, complete:Bool = false, sendEvent:Bool = true):Void {
-		
+
 		if (target != null) {
-			
+
 			if (Std.is (target, IGenericActuator)) {
-				
+
 				var actuator:IGenericActuator = cast target;
 				actuator.stop (null, complete, sendEvent);
-				
+
 			} else {
-				
+
 				var library = getLibrary (target, false);
-				
+
 				if (library != null) {
-					
+
 					if (Std.is (properties, String)) {
-						
+
 						var temp = { };
 						Reflect.setField (temp, properties, null);
 						properties = temp;
-						
+
 					} else if (Std.is (properties, Array)) {
-						
+
 						var temp = {};
-						
+
 						for (property in cast (properties, Array <Dynamic>)) {
-							
+
 							Reflect.setField (temp, property, null);
-							
+
 						}
-						
+
 						properties = temp;
-						
+
 					}
-					
+
 					var i = library.length - 1;
-					
+
 					while (i >= 0) {
-						
+
 						library[i].stop (properties, complete, sendEvent);
 						i--;
-						
+
 					}
-					
+
 				}
-				
+
 			}
-			
+
 		}
-		
+
 	}
-	
-	
+
+
 	/**
 	 * Creates a tween-based timer, which is useful for synchronizing function calls with other animations
 	 * @example		<code>Actuate.timer (1).onComplete (trace, [ "Timer is now complete" ]);</code>
@@ -366,14 +384,14 @@ class Actuate {
 	 * @return		The current actuator instance, which can be used to apply properties like onComplete or to gain a reference to the target timer object
 	 */
 	public static function timer (duration:Float, customActuator:Class<GenericActuator<TweenTimer>> = null):GenericActuator<TweenTimer> {
-		
+
 		return cast tween (new TweenTimer (0), duration, new TweenTimer (1), false, cast customActuator);
-		
+
 	}
-	
-	
+
+
 	#if (flash || nme || openfl)
-	
+
 	/**
 	 * Creates a new transform tween
 	 * @example		<code>Actuate.transform (MyClip, 1).color (0xFF0000);</code>
@@ -383,14 +401,14 @@ class Actuate {
 	 * @return		A TransformOptions instance, which is used to select the kind of transform you would like to apply to the target
 	 */
 	/*@:generic*/ public static function transform<T> (target:T, duration:Float = 0, overwrite:Bool = true):TransformOptions<T> {
-		
+
 		return new TransformOptions (target, duration, overwrite);
-		
+
 	}
-	
+
 	#end
-	
-	
+
+
 	/**
 	 * Creates a new tween
 	 * @example		<code>Actuate.tween (MyClip, 1, { alpha: 1 } ).onComplete (trace, [ "MyClip is now visible" ]);</code>
@@ -400,98 +418,98 @@ class Actuate {
 	 * @param	overwrite			Sets whether previous tweens for the same target and properties will be overwritten (Default is true)
 	 * @param	customActuator		A custom actuator to use instead of the default (Optional)
 	 * @return		The current actuator instance, which can be used to apply properties like ease, delay, onComplete or onUpdate
-	 */ 
+	 */
 	/*@:generic*/ public static function tween<T> (target:T, duration:Float, properties:Dynamic, overwrite:Bool = true, customActuator:Class<GenericActuator<T>> = null):GenericActuator<T> {
-		
+
 		if (target != null) {
-			
+
 			if (duration > 0) {
-				
+
 				if (customActuator == null) {
-					
+
 					customActuator = cast defaultActuator;
-					
+
 				}
-				
+
 				var actuator:GenericActuator<T> = Type.createInstance (customActuator, [ target, duration, properties ]);
 				var library = getLibrary (actuator.target);
-				
+
 				if (overwrite) {
-					
+
 					var i = library.length - 1;
-					
+
 					while (i >= 0) {
 						library[i].stop (actuator.properties, false, false);
 						i--;
 					}
-					
+
 					library = getLibrary (actuator.target);
-					
+
 				}
-				
+
 				library.push (actuator);
 				actuator.move ();
-				
+
 				return actuator;
-				
+
 			} else {
-				
+
 				return apply (target, properties, customActuator);
-				
+
 			}
-			
+
 		}
-		
+
 		return null;
-		
+
 	}
-	
-	
+
+
 	/*@:generic*/ public static function unload<T> (actuator:GenericActuator<T>):Void {
-		
+
 		var target = actuator.target;
-		
+
 		#if neko
-		
+
 		if (Reflect.isFunction (target)) {
-			
+
 			if (methodLibraries.exists (target)) {
-				
+
 				methodLibraries.get (target).remove (actuator);
-				
+
 				if (methodLibraries.get (target).length == 0) {
-					
+
 					methodLibraries.remove (target);
-					
+
 				}
-				
+
 			}
-			
+
 			return;
-			
+
 		}
-		
+
 		#end
-		
+
 		if (targetLibraries.exists (target)) {
-			
+
 			targetLibraries.get (target).remove (actuator);
-			
+
 			if (targetLibraries.get (target).length == 0) {
-				
+
 				targetLibraries.remove (target);
-				
+
 			}
-			
+
 		}
-		
+
 	}
-	
-	
+
+
 	/**
 	 * Creates a new tween that updates a method rather than setting the properties of an object
 	 * @example		<code>Actuate.update (trace, 1, ["Value: ", 0], ["", 1]).onComplete (trace, [ "Finished tracing values between 0 and 1" ]);</code>
-	 * @param	target		The method to update		
+	 * @param	target		The method to update
 	 * @param	duration		The length of the tween in seconds
 	 * @param	start		The starting parameters of the method call. You may use both numeric and non-numeric values
 	 * @param	end		The ending parameters of the method call. You may use both numeric and non-numeric values, but the signature should match the start parameters
@@ -499,14 +517,14 @@ class Actuate {
 	 * @return		The current actuator instance, which can be used to apply properties like ease, delay, onComplete or onUpdate
 	 */
 	/*@:generic*/ public static function update<T> (target:T, duration:Float, start:Array <Dynamic> = null, end:Array <Dynamic> = null, overwrite:Bool = true):GenericActuator<T> {
-				
+
 		var properties:Dynamic = { start: start, end: end };
-		
+
 		return tween (target, duration, properties, overwrite, MethodActuator);
-		
+
 	}
-	
-	
+
+
 }
 
 
@@ -525,22 +543,22 @@ import flash.geom.Matrix;
 #if (flash || nme || openfl)
 
 private class EffectsOptions {
-	
-	
+
+
 	private var duration:Float;
 	private var overwrite:Bool;
 	private var target:DisplayObject;
-	
-	
+
+
 	public function new (target:DisplayObject, duration:Float, overwrite:Bool) {
-		
+
 		this.target = target;
 		this.duration = duration;
 		this.overwrite = overwrite;
-		
+
 	}
-	
-	
+
+
 	/**
 	 * Creates a new BitmapFilter tween
 	 * @param	reference		A reference to the target's filter, which can be an array index or the class of the filter
@@ -548,34 +566,34 @@ private class EffectsOptions {
 	 * @return		The current actuator instance, which can be used to apply properties like ease, delay, onComplete or onUpdate
 	 */
 	public function filter (reference:Dynamic, properties:Dynamic):IGenericActuator {
-		
+
 		properties.filter = reference;
-		
+
 		return Actuate.tween (target, duration, properties, overwrite, FilterActuator);
-		
+
 	}
-	
-	
+
+
 }
 
 
 private class TransformOptions<T> {
-	
-	
+
+
 	private var duration:Float;
 	private var overwrite:Bool;
 	private var target:T;
-	
-	
+
+
 	public function new (target:T, duration:Float, overwrite:Bool) {
-		
+
 		this.target = target;
 		this.duration = duration;
 		this.overwrite = overwrite;
-		
+
 	}
-	
-	
+
+
 	/**
 	 * Creates a new ColorTransform tween
 	 * @param	color		The color value
@@ -584,20 +602,20 @@ private class TransformOptions<T> {
 	 * @return		The current actuator instance, which can be used to apply properties like ease, delay, onComplete or onUpdate
 	 */
 	public function color (value:Int = 0x000000, strength:Float = 1, alpha:Null <Float> = null):IGenericActuator {
-		
+
 		var properties:Dynamic = { colorValue: value, colorStrength: strength };
-		
+
 		if (alpha != null) {
-			
+
 			properties.colorAlpha = alpha;
-			
+
 		}
-		
+
 		return Actuate.tween (target, duration, properties, overwrite, TransformActuator);
-		
+
 	}
-	
-	
+
+
 	/**
 	 * Creates a new SoundTransform tween
 	 * @param	volume		The end volume for the target, or null if you would like to ignore this property (Default is null)
@@ -605,44 +623,44 @@ private class TransformOptions<T> {
 	 * @return		The current actuator instance, which can be used to apply properties like ease, delay, onComplete or onUpdate
 	 */
 	public function sound (volume:Null <Float> = null, pan:Null <Float> = null):IGenericActuator {
-		
+
 		var properties:Dynamic = {};
-		
+
 		if (volume != null) {
-			
+
 			properties.soundVolume = volume;
-			
+
 		}
-		
+
 		if (pan != null) {
-			
+
 			properties.soundPan = pan;
-			
+
 		}
-		
+
 		return Actuate.tween (target, duration, properties, overwrite, TransformActuator);
-		
+
 	}
-	
-	
+
+
 }
 
 #end
 
 
 private class TweenTimer {
-	
-	
+
+
 	public var progress:Float;
-	
-	
+
+
 	public function new (progress:Float):Void {
-		
+
 		this.progress = progress;
-		
+
 	}
-	
-	
+
+
 }
 
 
@@ -654,17 +672,17 @@ private class KVPairMap<K, V> {
 		this.keyList = [];
 		this.valList = [];
 	}
-	function equals(k1:K, k2:K):Bool 
+	function equals(k1:K, k2:K):Bool
 		return throw 'base';
-	
+
 	function indexOf(k:K):Int {
 		for (i in 0...keyList.length)
 			if (equals(k, keyList[i])) return i;
 		return -1;
 	}
-	public function get(k:K):Null<V> 
+	public function get(k:K):Null<V>
 		return valList[indexOf(k)];
-	
+
 	public function set(k:K, v:V):V {
 		var i = indexOf(k);
 		if (i == -1) {
@@ -677,9 +695,9 @@ private class KVPairMap<K, V> {
 		}
 		return v;
 	}
-	public function exists(k:K):Bool 
+	public function exists(k:K):Bool
 		return indexOf(k) != -1;
-	
+
 	public function remove(k:K):Bool {
 		var i = indexOf(k);
 		return
@@ -690,16 +708,16 @@ private class KVPairMap<K, V> {
 				true;
 			}
 	}
-	public inline function keys():Iterator<K> 
+	public inline function keys():Iterator<K>
 		return keyList.iterator();
-	
-	public inline function iterator():Iterator<V> 
+
+	public inline function iterator():Iterator<V>
 		return valList.iterator();
-	
+
 	public function toString() {
 		return [for (i in 0...keyList.length) Std.string(keyList[i]) => valList[i]].toString();
 	}
-	
+
 }
 
 private class FunctionMap < K, V > extends KVPairMap < K, V > {
